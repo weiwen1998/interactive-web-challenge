39028158b5384224963fcb5ec1a7c287c7352011
weiwen1998 <tanweiwen1998@gmail.com>
2022-05-26 17:00:07 +1000

    Updated files

--- interactive-web-challenge/plots.js
+++ interactive-web-challenge/plots.js
@@ -1,70 +1,72 @@
-// // Display the default plot
-// function init() {
-//   let data = [{
-//     values: sample_values,
-//     labels: otu_ids,
-//     hovertext: otu_labels
-//     type: "pie"
-//   }];
 
-//   let layout = {
-//     height: 600,
-//     width: 800
-//   };
-
-//   Plotly.newPlot("bar", data, layout);
-// }
-
-// // On change to the DOM, call getData()
-// d3.selectAll("#selDataset").on("change", getData);
-
-// // Function called by DOM changes
-// function getData() {
-//   let dropdownMenu = d3.select("#selDataset");
-//   // Assign the value of the dropdown menu option to a letiable
-//   let dataset = dropdownMenu.property("value");
-//   // Initialize an empty array for the country's data
-//   let data = [];
-
- 
-// // Call function to update the chart
-//   updatePlotly(data);
-// }
-
-// // Update the restyled plot's values
-// function updatePlotly(newdata) {
-//   Plotly.restyle("pie", "values", [newdata]);
-// }
-
-// init();
-function buildCharts(sample) {
+function buildCharts() {
     d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then((data) => {
       let samples = data.samples;
-      let resultArray = samples.filter(sampleObj => sampleObj.id == sample);
+      console.log(samples)
+      let resultArray = samples.filter(sampleObj => sampleObj.id == "940");
       let result = resultArray[0];
   
       let otu_ids = result.otu_ids;
       let otu_labels = result.otu_labels;
       let sample_values = result.sample_values;
-      
-Plotly.newPlot("bar", data, bubbleLayout);
 
-    let yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
-    let barData = [
-      {
-        y: yticks,
-        x: sample_values.slice(0, 10).reverse(),
-        text: otu_labels.slice(0, 10).reverse(),
-        type: "bar",
-        orientation: "h",
-      }
-    ];
+        let yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
+        let barData = [
+        {
+            y: yticks,
+            x: sample_values.slice(0, 10).reverse(),
+            text: otu_labels.slice(0, 10).reverse(),
+            type: "bar",
+            orientation: "h",
+        }
+        ];
 
-    let barLayout = {
-      title: "Top 10 Bacteria Cultures Found",
-      margin: { t: 30, l: 150 }
-    };
+        let barLayout = {
+          title: "Top 10 Bacteria Cultures Found",
+          margin: { t: 30, l: 150 }
+        };
 
     Plotly.newPlot("bar", barData, barLayout);
   });
 }
+buildCharts();
+
+function buildCharts2() {
+  d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then((data) => {
+    let samples = data.samples;
+    console.log(samples)
+    let resultArray = samples.filter(sampleObj => sampleObj.id == "940");
+    let result = resultArray[0];
+
+    let otu_ids = result.otu_ids;
+    let otu_labels = result.otu_labels;
+    let sample_values = result.sample_values;
+
+      let yticks = sample_values.map(otuID => `OTU ${otuID}`).reverse();
+      let bubbleData = [
+      {
+          y: sample_values,
+          x: otu_ids,
+          mode: 'markers',
+          marker: {
+            size: sample_values,
+            color: otu_ids
+          }
+      }
+      ];
+
+      let bubbleLayout = {
+        title: "Bubble Display of Samples",
+        margin: { t: 30, l: 150 }
+      };
+
+  Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+});
+}
+buildCharts2();
+
+const selOptions = [{value: sample_values, text: sample_values}]
+const selElement = d3.select("#selDataset")
+selOptions.forEach(d => {
+  selElement.append("option").attr("value", d.value).text(d.text)
+})
